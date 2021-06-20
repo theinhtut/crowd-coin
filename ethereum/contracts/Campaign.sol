bb2aa3cd1ad5cce0c61e434ad6951f33a6aab0e5
Thein Htut <theinhtut.dec@gmail.com>
2021-06-20 13:18:59 +0800

    Updated Campaign contract

--- ethereum/contracts/Campaign.sol
+++ ethereum/contracts/Campaign.sol
@@ -72,6 +72,26 @@ contract Campaign {
         request.recipient.transfer(request.value);
         request.complete = true;
     }
+
+    function getSummary() public view returns(
+        uint,
+        uint,
+        uint,
+        uint,
+        address
+        ) {
+        return(
+            minimumContribution,
+            address(this).balance,
+            requests.length,
+            approversCount,
+            manager
+        );
+    }
+    
+    function getRequestsCount() public view returns (uint){
+        return requests.length;
+    }
     
     modifier restricted() {
         require(msg.sender == manager);
