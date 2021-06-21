63815f7fe4d3f7ca4eb7a3088c0be5a46ec54346
Thein Htut <theinhtut.dec@gmail.com>
2021-06-21 13:20:53 +0800

    Added home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,37 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import Link from 'next/link'
 import factory from '../ethereum/factory'
 
-function CampaignIndex() {
+// Components
+import { Button } from 'semantic-ui-react'
+import CampaignsList from '../components/CampaignsList'
+import Layout from '../components/Layout'
+
+function CampaignIndex({ campaigns }) {
   const [web, setWeb] = useState('')
-  useEffect(async () => {
-    const campaigns = await factory.methods.getDeployedCampaigns().call()
-    console.log(campaigns)
-  }, [])
 
   return (
-    <div onClick={() => setWeb('hahahah')}>
-      <p>This is home page</p>
-    </div>
+    <Layout>
+      <div onClick={() => setWeb('hahahah')}>
+        <h2>Open Campaigns</h2>
+        <Link href='/campaigns/new'>
+          <a>
+            <Button floated='right' content='Create campaign' icon='add circle' primary />
+          </a>
+        </Link>
+        <CampaignsList campaigns={campaigns} />
+      </div>
+    </Layout>
   )
+}
+
+export async function getServerSideProps() {
+  const campaigns = await factory.methods.getDeployedCampaigns().call()
+  return {
+    props: {
+      campaigns,
+    },
+  }
 }
 
 export default CampaignIndex
